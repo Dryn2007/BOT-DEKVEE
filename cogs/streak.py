350b85f911672c6393605d0ec39346b814402cc2
Dryn2007 <adrianadiputra2007@gmail.com>
2026-07-19 21:24:28 +0700

    udpate

--- cogs/streak.py
+++ cogs/streak.py
@@ -135,7 +135,7 @@ class StreakSystem(commands.Cog):
         ''', prodi_name, today)
 
         # 3. Trigger saat mencapai pas 5 orang
-        if count == 5:
+        if count == 1:
             record = await self.bot.pool.fetchrow('SELECT current_streak, last_active_date FROM prodi_streaks WHERE prodi_name = $1', prodi_name)
 
             new_streak = 1
