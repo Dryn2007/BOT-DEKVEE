e8eae7ea20c490aa8816e2727645ce10239b3aa8
Dryn2007 <adrianadiputra2007@gmail.com>
2026-07-29 22:46:19 +0700

    updateee

--- cogs/music.py
+++ cogs/music.py
@@ -107,7 +107,8 @@ class Music(commands.Cog):
             'http_headers': {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
                 'Accept-Language': 'en-US,en;q=0.9',
-            }
+            },
+            'extractor_args': {'youtube': ['client=android,ios,web']}
         }
         self.FFMPEG_OPTIONS = {
             'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
