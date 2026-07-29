1da5fe1712aa8d1607fbd5e24fcacf865039bc57
Dryn2007 <adrianadiputra2007@gmail.com>
2026-07-29 23:57:37 +0700

    dateup

--- cogs/music.py
+++ cogs/music.py
@@ -97,18 +97,19 @@ class Music(commands.Cog):
             'restrictfilenames': True,
             'noplaylist': True,
             'nocheckcertificate': True,
-            'ignoreerrors': False,
+            'ignoreerrors': True,
             'logtostderr': False,
             'quiet': True,
             'no_warnings': True,
             'default_search': 'auto',
             'source_address': '0.0.0.0',
             'cookiefile': 'cookies.txt',
+            # MANIPULASI TINGKAT LANJUT
+            'extractor_args': {'youtube': ['client=tv,android,ios']},
             'http_headers': {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
                 'Accept-Language': 'en-US,en;q=0.9',
-            },
-            'extractor_args': {'youtube': ['client=android,ios,web']}
+            }
         }
         self.FFMPEG_OPTIONS = {
             'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
